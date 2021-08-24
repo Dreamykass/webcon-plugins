81e795a26e9719b1886e42c6335df87abb708abc
Damian Grüner (Dreamykass) <grunerdamian@gmail.com>
2021-08-24 12:54:36 +0200

    dire roll for next step

---
+++ DiceRollForNextStep/DiceRollForNextStep/DiceRollForNextStepAction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using WebCon.WorkFlow.SDK.ActionPlugins;
+using WebCon.WorkFlow.SDK.ActionPlugins.Model;
+
+namespace DiceRollForNextStep
+{
+	public class DiceRollForNextStepAction : CustomAction<DiceRollForNextStepActionConfig>
+	{
+		public override void Run(RunCustomActionParams args)
+		{
+			void log(string msg) => args.Context.PluginLogger.AppendInfo(msg);
+
+			log("DiceRollForNextStepAction");
+			log($"current workflow id: {args.Context.CurrentWorkflowID}");
+			log($"current process id: {args.Context.CurrentProcessID}");
+			log($"current document step id: {args.Context.CurrentDocument.StepID}");
+			log($"success path id: {Configuration.SuccessPathId}");
+			log($"fail path id: {Configuration.FailPathId}");
+
+			var roll = new Random().Next(1, 7); // [1,2,3,4,5,6]
+			log($"rolled: {roll}");
+
+			if (roll == 6)
+			{
+				log("rolled a 6, going to success step");
+				MoveDocumentToNextStep(args.Context.CurrentDocument, Configuration.SuccessPathId);
+			}
+			else
+			{
+				log("didn't roll a 6, going to fail step");
+				MoveDocumentToNextStep(args.Context.CurrentDocument, Configuration.FailPathId);
+			}
+
+		}
+
+		private void MoveDocumentToNextStep(WebCon.WorkFlow.SDK.Documents.Model.ExistingDocumentData existingDocumentData, int pathId)
+		{
+			WebCon.WorkFlow.SDK.Documents.DocumentsManager.MoveDocumentToNextStep(
+				new WebCon.WorkFlow.SDK.Documents.Model.MoveDocumentToNextStepParams(existingDocumentData, pathId));
+		}
+	}
+}
---
+++ DiceRollForNextStep/DiceRollForNextStep/DiceRollForNextStepActionConfig.cs
@@ -0,0 +1,14 @@
+﻿using WebCon.WorkFlow.SDK.Common;
+using WebCon.WorkFlow.SDK.ConfigAttributes;
+
+namespace DiceRollForNextStep
+{
+	public class DiceRollForNextStepActionConfig : PluginConfiguration
+	{
+		[ConfigEditableText(DisplayName = "Success Path ID (to go to, if we roll a 6)", IsRequired = true)]
+		public int SuccessPathId { get; set; }
+
+		[ConfigEditableText(DisplayName = "Fail Path ID (to go to, if we don't roll a 6)", IsRequired = true)]
+		public int FailPathId { get; set; }
+	}
+}
