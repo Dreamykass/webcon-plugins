7dca3ea997fa72ca1d8a5ea1024efeaa819104c2
Damian Grüner (Dreamykass) <grunerdamian@gmail.com>
2021-08-27 09:32:32 +0200

    sleep action

---
+++ Sleep/Sleep/SleepAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using WebCon.WorkFlow.SDK.ActionPlugins;
+using WebCon.WorkFlow.SDK.ActionPlugins.Model;
+
+namespace Sleep
+{
+	public class SleepAction : CustomAction<SleepActionConfig>
+	{
+		public override void Run(RunCustomActionParams args)
+		{
+			void log(string msg) => args.Context.PluginLogger.AppendInfo(msg);
+
+			log($"Sleeping for {Configuration.Seconds} seconds...");
+			System.Threading.Thread.Sleep(Configuration.Seconds * 1000);
+			log("Finished sleeping.");
+		}
+	}
+}
---
+++ Sleep/Sleep/SleepActionConfig.cs
@@ -0,0 +1,11 @@
+﻿using WebCon.WorkFlow.SDK.Common;
+using WebCon.WorkFlow.SDK.ConfigAttributes;
+
+namespace Sleep
+{
+	public class SleepActionConfig : PluginConfiguration
+	{
+		[ConfigEditableInteger(DisplayName = "Number of seconds to sleep for.")]
+		public int Seconds { get; set; } = 30;
+	}
+}
