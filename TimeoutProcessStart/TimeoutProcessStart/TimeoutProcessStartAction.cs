8fdaaef49aa3b926e20321cd5e4e1f5c6694f4de
Damian Grüner (Dreamykass) <grunerdamian@gmail.com>
2021-09-13 14:58:54 +0200

    timeoutprocessstart

---
+++ TimeoutProcessStart/TimeoutProcessStart/TimeoutProcessStartAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using WebCon.WorkFlow.SDK.ActionPlugins;
+using WebCon.WorkFlow.SDK.ActionPlugins.Model;
+
+namespace TimeoutProcessStart
+{
+	public class TimeoutProcessStartAction : CustomAction<TimeoutProcessStartActionConfig>
+	{
+
+		public override void RunWithoutDocumentContext(RunCustomActionWithoutContextParams args)
+		{
+			void log(string msg) => args.Context.PluginLogger.AppendInfo(msg);
+
+			log("TimeoutProcessStartAction hello");
+
+
+			var docTypeId = 1; // 0 errors out as invalid document type
+			var workFlowId = args.Context.CurrentWorkflowID;
+			var newDocData = WebCon.WorkFlow.SDK.Documents.DocumentsManager.GetNewDocument(
+				new WebCon.WorkFlow.SDK.Documents.Model.GetNewDocumentParams(docTypeId, workFlowId)
+			);
+			var path = 0;
+
+			log("created a new doc");
+
+			WebCon.WorkFlow.SDK.Documents.DocumentsManager.StartNewWorkFlow(
+				new WebCon.WorkFlow.SDK.Documents.Model.StartNewWorkFlowParams(
+					newDocData, path
+				)
+			);
+
+			log("started the workflow");
+		}
+	}
+}
---
+++ TimeoutProcessStart/TimeoutProcessStart/TimeoutProcessStartActionConfig.cs
@@ -0,0 +1,20 @@
+﻿using WebCon.WorkFlow.SDK.Common;
+using WebCon.WorkFlow.SDK.ConfigAttributes;
+
+namespace TimeoutProcessStart
+{
+	public class TimeoutProcessStartActionConfig : PluginConfiguration
+	{
+		//[ConfigEditableText(DisplayName = "Source form field ID")]
+		//public string SourceFormFieldID { get; set; }
+
+		//[ConfigEditableText(DisplayName = "Destination form field ID")]
+		//public string DestinationFormFieldID { get; set; }
+
+		//[ConfigEditableText(DisplayName = "Price form field ID", Description = "ID of the form field which contains price of the product")]
+		//public string PriceFormFieldID { get; set; }
+
+		//[ConfigEditableInteger(DisplayName = "Price")]
+		//public int Price { get; set; }
+	}
+}
